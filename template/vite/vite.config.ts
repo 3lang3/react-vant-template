f1f59bd8b17e05b49a3fc034136406688c7a8ca0
3lang <675483520@qq.com>
2021-09-18 18:05:20 +0800

    feat: add vite temp

---
+++ template/vite/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import styleImport from "vite-plugin-style-import";
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          "brand-color": "red",
+          "font-size-sm": "13px",
+          "font-size-md": "15px",
+          "font-size-lg": "17px",
+          "action-bar-button-danger-color": "#7232dd",
+          "action-bar-button-warning-color": "#3eaf7c",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: /^~/, replacement: "" }],
+  },
+  plugins: [reactRefresh(), styleImport({
+    libs: [
+      {
+        libraryName: "react-vant",
+        resolveStyle: (name) => `react-vant/es/${name}/style/index.less`,
+      },
+    ],
+  }),]
+})
