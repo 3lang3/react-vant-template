ec6423e79543803a3c430f33f9196db3a6bc62f6
3lang <675483520@qq.com>
2021-10-28 17:53:48 +0800

    chore: cra add dropdownmenu test

--- template/create-react-app/src/App.tsx
+++ template/create-react-app/src/App.tsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { Button, Popup, Search, DropdownMenu } from 'react-vant';
+import type { DropdownMenuInstance } from 'react-vant/es/dropdown-menu';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Popup, Search } from 'react-vant';
-import { checkResponse } from './utils';
+
+const option1 = [
+  { text: '全部商品', value: 0 },
+  { text: '新款商品', value: 1 },
+  { text: '活动商品', value: 2 },
+];
+const option2 = [
+  { text: '默认排序', value: 'a' },
+  { text: '好评排序', value: 'b' },
+  { text: '销量排序', value: 'c' },
+];
 
 function App() {
   const [v, set] = useState(true)
+  const [value, setValue] = useState<Record<string, string | number>>({});
+  const ref = useRef<DropdownMenuInstance>(null);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <DropdownMenu ref={ref} value={value} onChange={(v) => setValue(v)}>
+          <DropdownMenu.Item name="value1" options={option1} />
+          <DropdownMenu.Item name="value2" options={option2} />
+        </DropdownMenu>
+        <Button size="small" type="info" onClick={() => ref.current?.showItem(1)}>打开dropdown</Button>
         <Button round type="primary" onClick={() => set(true)}>欢迎使用React Vant</Button>
-        <Button round type="primary" onClick={() => checkResponse({ type: 1, message: "请求错误" })}>测试Notify</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
