26b81007917b6e9ccc6ea1a4fee49883f0b09662
3lang <675483520@qq.com>
2021-11-04 18:43:18 +0800

    chore: rm debug code

--- template/vite/vite.config.ts
+++ template/vite/vite.config.ts
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import styleImport from "vite-plugin-style-import";
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import visualizer from "rollup-plugin-visualizer";
-
-const plugins = [];
- 
-// 打包生产环境才引入的插件
-if (process.env.NODE_ENV === "production") {
-    // 打包依赖展示
-    plugins.push(
-        visualizer({
-          open: true,
-            gzipSize: true,
-            brotliSize: true,
-        })
-    );
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -37,5 +22,12 @@ export default defineConfig({
   resolve: {
     alias: [{ find: /^~/, replacement: "" }],
   },
-  plugins: [reactRefresh(), ...plugins]
+  plugins: [reactRefresh(), styleImport({
+    libs: [
+      {
+        libraryName: "react-vant",
+        resolveStyle: (name) => `react-vant/es/${name}/style`,
+      },
+    ],
+  })]
 })
