232227f7a351f9d9fc908dbbadd9729fe21530e9
3lang <675483520@qq.com>
2022-02-17 20:16:16 +0800

    feat: add remax demo test

---
+++ next/remix/app/routes/index.tsx
@@ -0,0 +1,34 @@
+import { Button } from 'react-vant/dist/patch'
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <Button>Welcome to Remix</Button>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
