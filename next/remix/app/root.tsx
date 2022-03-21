70e04677d9f56ff098b2fb4ac9e7f0f7e62816b2
3lang <675483520@qq.com>
2022-03-21 10:41:21 +0800

    feat: add next demo (remixjs)

--- next/remix/app/root.tsx
+++ next/remix/app/root.tsx
@@ -4,20 +4,25 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import styles from 'react-vant/lib/index.css'
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
