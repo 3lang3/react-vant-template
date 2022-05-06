73ede05cec7708a7acfc0a9c176da1df8e0e7b7d
3lang <675483520@qq.com>
2022-05-06 15:35:51 +0800

    chore: update rc18

--- next/cra/src/App.tsx
+++ next/cra/src/App.tsx
@@ -1,11 +1,16 @@
-import { Arrow, Like } from '@react-vant/icons';
-import { Card, Image, Button, Toast, Space, Typography, Tag } from 'react-vant';
+import { Arrow, Like } from "@react-vant/icons";
+import { Card, Image, Button, Toast, Space, Typography, Tag } from "react-vant";
 
 export default function App() {
   return (
     <Space
       direction="vertical"
-      style={{ padding: 20, width: '100%', backgroundColor: '#f2f2f2', boxSizing: 'border-box' }}
+      style={{
+        padding: 20,
+        width: "100%",
+        backgroundColor: "#f2f2f2",
+        boxSizing: "border-box",
+      }}
     >
       <Typography.Title level={1}>
         Hello React Vant <Tag type="primary">next</Tag>
@@ -14,16 +19,16 @@ export default function App() {
         参照 Vant 打造的 React 框架移动端组件库。
       </Typography.Text>
       <Card round>
-        <Card.Cover onClick={() => Toast.info('点击了Cover区域')}>
+        <Card.Cover onClick={() => Toast.info("点击了Cover区域")}>
           <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/baa20698931623.5ee79b6a8ec2b.jpg" />
         </Card.Cover>
         <Card.Header
           extra={<Arrow />}
-          onClick={() => Toast.info('点击了Header区域')}
+          onClick={() => Toast.info("点击了Header区域")}
         >
           封面展示
         </Card.Header>
-        <Card.Body onClick={() => Toast.info('点击了Body区域')}>
+        <Card.Body onClick={() => Toast.info("点击了Body区域")}>
           卡片内容区域
         </Card.Body>
         <Card.Footer>
@@ -45,14 +50,14 @@ export default function App() {
       <Card round>
         <Card.Header
           extra={<Arrow />}
-          onClick={() => Toast.info('点击了Header区域')}
+          onClick={() => Toast.info("点击了Header区域")}
         >
           封面展示
         </Card.Header>
-        <Card.Body onClick={() => Toast.info('点击了Body区域')}>
+        <Card.Body onClick={() => Toast.info("点击了Body区域")}>
           卡片内容区域
         </Card.Body>
-        <Card.Cover onClick={() => Toast.info('点击了Cover区域')}>
+        <Card.Cover onClick={() => Toast.info("点击了Cover区域")}>
           <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/274faa127009547.61390144590a7.png" />
         </Card.Cover>
       </Card>
