67c5a49ec6fe0ed0d5f8aec6ed28f57bd727a5ac
3lang <675483520@qq.com>
2022-07-11 18:48:21 +0800

    chore: update v3

--- next/umijs/src/pages/index.tsx
+++ next/umijs/src/pages/index.tsx
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
-import { Popup, Cell } from 'react-vant';
+import { Field } from 'react-vant';
 
+// Field 是基于 Cell 实现的，可以使用 Cell.Group 作为容器来提供外边框。
 export default () => {
-  const [visible, setVisible] = useState(false);
-
+  const [value, setValue] = useState('');
   return (
-    <>
-      <Cell title="展示弹出层" isLink onClick={() => setVisible(true)} />
-      <Popup
-        visible={visible}
-        onClose={() => {
-          setVisible(false);
-        }}
-      >
-        <div style={{ padding: '30px 50px' }}>内容</div>
-      </Popup>
-    </>
+    <Field
+      label="文本"
+      tooltip="提示tooltip"
+      intro={<div>We must make sure that your are a human.</div>}
+      placeholder="请输入文本"
+      value={value}
+      onChange={setValue}
+    />
   );
 };
--- next/vite/src/App.tsx
+++ next/vite/src/App.tsx
@@ -1,29 +1,66 @@
-import React from 'react';
-import { Notify, Cell } from 'react-vant';
+import { Arrow, Like } from "@react-vant/icons";
+import { Card, Image, Button, Toast, Space, Typography, Tag } from "react-vant";
 
-export default () => {
+export default function App() {
   return (
-    <>
-      <Cell
-        title="主要通知"
-        isLink
-        onClick={() => Notify.show({ type: 'primary', message: '通知内容' })}
-      />
-      <Cell
-        title="成功通知"
-        isLink
-        onClick={() => Notify.show({ type: 'success', message: '通知内容' })}
-      />
-      <Cell
-        title="危险通知"
-        isLink
-        onClick={() => Notify.show({ type: 'danger', message: '通知内容' })}
-      />
-      <Cell
-        title="警告通知"
-        isLink
-        onClick={() => Notify.show({ type: 'warning', message: '通知内容' })}
-      />
-    </>
+    <Space
+      direction="vertical"
+      style={{
+        padding: 20,
+        width: "100%",
+        backgroundColor: "#f2f2f2",
+        boxSizing: "border-box",
+      }}
+    >
+      <Typography.Title level={1}>
+        Hello React Vant <Tag type="primary">next</Tag>
+      </Typography.Title>
+      <Typography.Text>
+        参照 Vant 打造的 React 框架移动端组件库。
+      </Typography.Text>
+      <Card round>
+        <Card.Cover onClick={() => Toast.info("点击了Cover区域")}>
+          <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/baa20698931623.5ee79b6a8ec2b.jpg" />
+        </Card.Cover>
+        <Card.Header
+          extra={<Arrow />}
+          onClick={() => Toast.info("点击了Header区域")}
+        >
+          封面展示
+        </Card.Header>
+        <Card.Body onClick={() => Toast.info("点击了Body区域")}>
+          卡片内容区域
+        </Card.Body>
+        <Card.Footer>
+          <Space>
+            <Button round size="small">
+              更多
+            </Button>
+            <Button
+              icon={<Like />}
+              round
+              color="linear-gradient(to right, #ff6034, #ee0a24)"
+              size="small"
+            >
+              Like
+            </Button>
+          </Space>
+        </Card.Footer>
+      </Card>
+      <Card round>
+        <Card.Header
+          extra={<Arrow />}
+          onClick={() => Toast.info("点击了Header区域")}
+        >
+          封面展示
+        </Card.Header>
+        <Card.Body onClick={() => Toast.info("点击了Body区域")}>
+          卡片内容区域
+        </Card.Body>
+        <Card.Cover onClick={() => Toast.info("点击了Cover区域")}>
+          <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/274faa127009547.61390144590a7.png" />
+        </Card.Cover>
+      </Card>
+    </Space>
   );
-};
+}
